5d88a7e939bcce44c6962d6d0796fb731529c551
Cameron Moorehead <hello@cameronmoorehead.com>
2018-03-08 13:16:19 -0800

    video proof of concept playing

--- frontend/src/components/genre-view/index.js
+++ frontend/src/components/genre-view/index.js
@@ -1,14 +1,17 @@
+import './_genre-view.scss';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Player } from 'video-react';
 
 const GenreView = ({ movies }) => {
   return (
     <div>
       Genres
-      {movies.map(movie => {
-        console.log(movie);
-        <img src={movie.movieURL} />;
-      })}
+      <Player
+        playsInline
+        poster='https://s3-us-west-2.amazonaws.com/simplex-cinema/6524a5c913bf8e862d967ce0a8b45670.black-panther-poster.jpeg'
+        src='https://s3-us-west-2.amazonaws.com/simplex-cinema/2fbf3ca371a7dbeef7dac3efcab72d9b.black-panther-trailer.mov'
+      />
     </div>
   );
 };
